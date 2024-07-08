bf68dc91efcffe7117a12e024e3d7df428f28c6b
hyunerase <wldn0716@icloud.com>
2024-07-09 04:06:49 +0900

    commit

--- community/settings.py
+++ community/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['port-0-community-lydc4xsn02460fa6.sel5.cloudtype.app','127.0.0.1']
+ALLOWED_HOSTS = []
 
 
 # Application definition
